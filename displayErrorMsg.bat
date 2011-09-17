9f37d7762d0a8565f32e75c006dbf1da47b35713
Kalycito <info@kalycito.com>
2011-09-17 07:53:37 +0000

    [TASK] Update C++ library and wrapper

--- displayErrorMsg.bat
+++ displayErrorMsg.bat
@@ -1,5 +1,7 @@
 @echo on
 @echo Error! ActiveTcl not installed
 @echo ActiveTcl can be downloaded at http://www.activestate.com/activetcl
+@echo NOTE: You always need to install the 32bit variant in order to run openCONFIGURATOR!
+@echo For Windows 7 users: start the TCL/TK setup with Run as Administrator... 
 @echo off
 PAUSE
